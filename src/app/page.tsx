7b36a5900a6a8f24d17cb0995f1af791a10a0e45
Antonio Sandoval <antonio@experiencetrue.com>
2025-04-05 14:03:19 -0700

    Update the UI to use bootstrap, updated the GET API to support filtering (optional), added a POST API request to insert new advocates (admin's only!), created a better table/grid interaction for the end-user which includes live query filtering, and a result counter, added a modal with detailed information about the advocate to help break away from the clutter of having all the data displayed on the table, cleaned up some of the general code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,322 @@
 "use client";
 
+import { Advocate, advocates } from "@/db/schema";
 import { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Col,
+  Container,
+  Input,
+  ListGroup,
+  ListGroupItem,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row,
+  Table,
+} from "reactstrap";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
+  const [queryValue, setQueryValue] = useState<string | null>(null);
+  const [selectedAdvocate, setSelectedAdvocate] = useState<Advocate | null>(
+    null
+  );
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
-  useEffect(() => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const advocateRequest = () => {
     console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
+    fetch(
+      queryValue === null
+        ? "/api/advocates"
+        : `/api/advocates?filter=${queryValue.toLowerCase()}`
+    ).then((response) => {
       response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
         setFilteredAdvocates(jsonResponse.data);
       });
     });
-  }, []);
-
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
   };
 
-  const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
+  const insertAdvocate = () => {
+    if (selectedAdvocate !== null) {
+      fetch("/api/advocates", {
+        method: "POST",
+        body: JSON.stringify({
+          ...selectedAdvocate,
+        }),
+      }).then((resp) => {
+        resp.json().then(() => {
+          advocateRequest();
+          toggle();
+        });
+      });
+    }
   };
+
+  const setAdvocateSelection = (advocate: Advocate) => {
+    setSelectedAdvocate(advocate);
+    toggle();
+  };
+
+  const addAdvocate = () => {
+    setSelectedAdvocate(null);
+    toggle();
+  };
+
+  const updateAdvocate = (propertyName: string, value: any) => {
+    //const updatedAdvocate = {...selectedAdvocate}
+    if (selectedAdvocate === null) {
+      const newAdvocate: Advocate = {
+        id: null,
+        firstName: null,
+        lastName: null,
+        city: null,
+        degree: null,
+        yearsOfExperience: null,
+        phoneNumber: null,
+        specialties: [],
+      };
+      const udpatedNewAdvocate = { ...newAdvocate, [propertyName]: value };
+      setSelectedAdvocate(udpatedNewAdvocate);
+    } else {
+      setSelectedAdvocate({ ...selectedAdvocate, [propertyName]: value });
+    }
+  };
+
+  useEffect(() => {
+    advocateRequest();
+  }, [queryValue]);
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
-      </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </main>
+    <>
+      <Container fluid>
+        <Row className="mt-2">
+          <Col>
+            <h3 className="display-3">Solace - Advocates</h3>
+          </Col>
+        </Row>
+        <Row className="mt-2">
+          <Col>
+            <Card className="solace_border">
+              <CardBody>
+                <CardTitle>
+                  <Row>
+                    <Col xs={5} sm={5} md={3} lg={2} xl={2}>
+                      <Input
+                        placeholder={"Search Advocate"}
+                        onChange={(e) => {
+                          setQueryValue(e?.target?.value ?? null);
+                        }}
+                      />
+                    </Col>
+                    <Col xs={3} sm={3} md={2} lg={2} xl={1} className="p-1">
+                      Results: {filteredAdvocates.length}
+                    </Col>
+                    <Col xs={1} sm={1} md={2} lg={7} xl={7}></Col>
+                    <Col xs={5} sm={5} md={3} lg={2} xl={2}>
+                      {isAdmin && (
+                        <Button
+                          className="solace"
+                          block
+                          onClick={() => addAdvocate()}
+                        >
+                          Add Advocate
+                        </Button>
+                      )}
+                    </Col>
+                  </Row>
+                </CardTitle>
+                <div style={{ height: "500px" }}>
+                  <Table
+                    // borderless
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      height: "100%",
+                    }}
+                  >
+                    <thead style={{ display: "block" }}>
+                      <tr style={{ display: "flex" }}>
+                        <th style={{ flex: "1" }}>First Name</th>
+                        <th style={{ flex: "1" }}>Last Name</th>
+                        <th style={{ flex: "1" }}>City</th>
+                        <th style={{ flex: "1" }}>Degree</th>
+                        {/* <th style={{ flex: "1" }}>Specialties</th> */}
+                        <th style={{ flex: "1" }}>Years of Experience</th>
+                        <th style={{ flex: "1" }}>Phone Number</th>
+                        <th style={{ flex: "1" }}>Option</th>
+                      </tr>
+                    </thead>
+                    <tbody style={{ display: "block", overflowX: "auto" }}>
+                      {(filteredAdvocates ?? []).map((advocate, index) => {
+                        return (
+                          <tr style={{ display: "flex" }} key={index}>
+                            <td style={{ flex: "1" }}>{advocate.firstName}</td>
+                            <td style={{ flex: "1" }}>{advocate.lastName}</td>
+                            <td style={{ flex: "1" }}>{advocate.city}</td>
+                            <td style={{ flex: "1" }}>{advocate.degree}</td>
+                            {/* <td style={{ flex: "1" }}>
+                              {advocate.specialties.map((s) => (
+                                <div>{s}</div>
+                              ))}
+                            </td> */}
+                            <td style={{ flex: "1" }}>
+                              {advocate.yearsOfExperience}
+                            </td>
+                            <td style={{ flex: "1" }}>
+                              {advocate.phoneNumber}
+                            </td>
+                            <td style={{ flex: "1" }}>
+                              <Button
+                                className="solace"
+                                block
+                                onClick={() => setAdvocateSelection(advocate)}
+                              >
+                                Details
+                              </Button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </Table>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>
+          {selectedAdvocate === null ? (
+            <h5>Let's add an Advocate</h5>
+          ) : (
+            `${selectedAdvocate?.firstName}'s Details`
+          )}
+        </ModalHeader>
+        <ModalBody>
+          <Row>
+            <Col md={6}>
+              <Input
+                placeholder={"First Name"}
+                defaultValue={selectedAdvocate?.firstName ?? ""}
+                onChange={(e) => {
+                  updateAdvocate("firstName", e?.target?.value ?? "");
+                }}
+              />
+            </Col>
+            <Col md={6}>
+              <Input
+                placeholder={"Last Name"}
+                defaultValue={selectedAdvocate?.lastName ?? ""}
+                onChange={(e) => {
+                  updateAdvocate("lastName", e?.target?.value ?? "");
+                }}
+              />
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col md={6}>
+              <Input
+                placeholder={"City"}
+                defaultValue={selectedAdvocate?.city ?? ""}
+                onChange={(e) => {
+                  updateAdvocate("city", e?.target?.value ?? "");
+                }}
+              />
+            </Col>
+            <Col md={6}>
+              <Input
+                placeholder={"Degree"}
+                defaultValue={selectedAdvocate?.degree ?? ""}
+                onChange={(e) => {
+                  updateAdvocate("degree", e?.target?.value ?? "");
+                }}
+              />
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col md={6}>
+              <Input
+                placeholder={"Years of Experience"}
+                defaultValue={selectedAdvocate?.yearsOfExperience ?? ""}
+                onChange={(e) => {
+                  updateAdvocate("yearsOfExperience", e?.target?.value ?? "");
+                }}
+              />
+            </Col>
+            <Col md={6}>
+              <Input
+                placeholder={"Phone Number"}
+                defaultValue={selectedAdvocate?.phoneNumber ?? ""}
+                onChange={(e) => {
+                  updateAdvocate("phoneNumber", e?.target?.value ?? "");
+                }}
+              />
+            </Col>
+          </Row>
+
+          <hr />
+          <Row>
+            <Col>
+              {selectedAdvocate === null ? (
+                <h5>Select Specialties</h5>
+              ) : (
+                <h5>{selectedAdvocate?.firstName ?? ""}'s Specialties</h5>
+              )}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ListGroup>
+                {selectedAdvocate?.specialties.map((spec) => {
+                  return <ListGroupItem>{spec}</ListGroupItem>;
+                })}
+              </ListGroup>
+            </Col>
+          </Row>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            className="solace"
+            onClick={() =>
+              window.open(
+                "https://youtu.be/dQw4w9WgXcQ?si=OH8XIXP9wgx7qkOR&t=43",
+                "_blank"
+              )
+            }
+          >
+            Chat with {selectedAdvocate?.firstName ?? ""} Live
+          </Button>
+          {isAdmin && (
+            <Button className="solace" onClick={() => insertAdvocate()}>
+              Insert Advocate
+            </Button>
+          )}
+
+          <Button className="solace_border" color={"white"} onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+
+      <Row className="mt-5 m-1">
+        <Col>
+          <Button color="danger" onClick={() => setIsAdmin(true)}>
+            Secret Admin Button - DO NOT PUSH
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 }
